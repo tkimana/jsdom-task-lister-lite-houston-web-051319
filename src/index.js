ceb3d1e0b185700368a6a6c11a30b3e012dcfcdb
Theodore Kimana <kimana.theodore@gmail.com>
2019-08-05 13:52:42 -0500

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+let form = document.querySelector('#create-task-form')
+form.addEventListener('submit', function(e){
+  e.preventDefault()
+  console.log('Hello World!!')
+let input = document.querySelector('#new-task-description')
+let ul= document.querySelector('#tasks')
+let li = document.createElement('li')
+let button = document.createElement('button')
+// Delete function needs to be passed  back here
+// createEventListener(button)
+ul.append(li)
+li.innerText = input.value
+li.append(button)
+button.innerText="Delete"
+button.style = "color: blue"
+//Create an EventListener to the delete button
+// function createEventListener(button){
+  button.addEventListener('click', function(e){
+     e.preventDefault()
+     button.parentElement.remove()
+    
+  })
+
+  // li.sort=(a,b)=> {
+  //   a - b
+  // }
+  // }
+input.value=""
+})
+
+})
+
+
+
